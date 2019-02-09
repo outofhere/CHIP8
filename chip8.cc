8b257d73b962527c9277036777080fee11406ec6
outofhere <outofhere@mail.ru>
2019-02-09 22:03:48 +0300

    Fistful of opcodes. Couple of programms to test them

--- chip8.cc
+++ chip8.cc
@@ -32,80 +32,203 @@ uint16_t chip8::opcodeFetch(uint8_t * memLocation) {
 void chip8::opcodeDecode(uint16_t opcode) {
     printf ("[!] Executing opcode: %#0.4x\n", opcode);
     switch(opcode & 0xF000) {
-        case 0x0000:
+        case 0x0000: {
         // Opcode: 0NNN
-        // Type: Call
         // Explanation: Calls RCA 1802 program at address NNN. Not necessary for most ROMs.
 
         // Opcode: 00E0
-        // Type: Display disp_clear()
         // Explanation: Clears the screen.
 
         // Opcode: 00EE
-        // Type: Flow return;
         // Explanation: Returns from a subroutine.
             if (opcode == 0x00EE) {
-                printf ("[!] RET instruction\n");
+                printf ("[!] ret\n");
                 pc = stack.at(sp);
                 --sp;
             } else if (opcode == 0x00E0) {
-                printf ("[!] CLRSCR instruction\n");
+                printf ("[!] clrscr\n");
                 pc += 2;
             } else {
-                printf ("[!] CALL instruction\n");
                 ++sp;
                 pc += 2;
                 stack.at(sp) = pc;
                 pc = opcode;
+                printf ("[!] call %#0.4x\n", pc);
             }
             break;
-
+        }
         // Type: 1NNN
-        // Type: Flow goto NNN;
         // Explanation: Jumps to address NNN.
-        case 0x1000:
-            printf ("[!] JMP instruction!\n");
+        case 0x1000: {
             pc = opcode & 0x0FFF;
+            printf ("[!] jmp %#0.4x\n", pc);
             break;
+        }
 
         // Type: 2NNN
-        // Type: Flow *(0xNNN)()
         // Explanation: Calls subroutine at NNN.
-        case 0x2000:
-            printf ("[!] CALL instruction\n");
+        case 0x2000: {
             ++sp;
             pc += 2;
             stack.at(sp) = pc;
             pc = opcode & 0x0FFF;
+            printf ("[!] call %#0.4x\n", pc);
             break;
+        }
 
         // Type: 3XNN
-        // Type: Cond if(Vx!=NN)
         // Explanation: Skips the next instruction if VX equals NN. (Usually the next instruction is a jump to skip a code block)
-        case 0x3000:
-            printf ("[!] Success!\n");
-            if (V.at((opcode & 0x0F00) >> 8) == opcode & 0x00FF) {pc += 0x4;}
+        case 0x3000: {
+            printf ("[!] je(v%x, %#x) 0x4\n", (opcode & 0x0F00) >> 8, opcode & 0x00FF);
+            if (V.at((opcode & 0x0F00) >> 8) == (uint8_t)opcode & 0x00FF) {
+                pc += 0x4;
+            } else {
+                pc += 0x2;
+            }
             break;
+        }
 
         // Type: 4XNN
-        // Type: Cond if(Vx==NN)
-        // Explanation: Skips the next instruction if VX equals NN. (Usually the next instruction is a jump to skip a code block)
-        case 0x4000:
-            printf ("[!] Success!\n");
-            if (V.at((opcode & 0x0F00) >> 8) != opcode & 0x00FF) {pc += 0x4;}
+        // Explanation: Skips the next instruction if VX doesn't equal NN. (Usually the next instruction is a jump to skip a code block)
+        case 0x4000: {
+            printf ("[!] jne(v%x, %#x) 0x4\n", (opcode & 0x0F00) >> 8, opcode & 0x00FF);
+            if (V.at((opcode & 0x0F00) >> 8) != (uint8_t)opcode & 0x00FF) {
+                pc += 0x4;
+            } else {
+                pc += 0x2;
+            }
             break;
+        }
 
         // Type: 5XY0
-        // Type: Cond if(Vx!=Vy)
         // Explanation: Skips the next instruction if VX equals VY. (Usually the next instruction is a jump to skip a code block
-        case 0x5000:
+        case 0x5000: {
+            printf ("[!] je(v%x, v%x) 0x4\n", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 8);
+            if (V.at((opcode & 0x0F00) >> 8) == V.at((opcode & 0x00F0) >> 8)) {
+                pc += 0x4;
+            } else {
+                pc += 0x2;
+            }
+            break;
+        }
+
+        // Type: 6XNN
+        // Explanation: Sets VX to NN
+        case 0x6000: {
+            printf ("[!] mov v%x, %#x\n", (opcode & 0x0F00) >> 8, opcode & 0x00FF);
+            V.at((opcode & 0x0F00) >> 8) = opcode & 0x00FF;
+            pc += 0x2;
+            break;
+        }
+
+        // Type: 7XNN
+        // Explanation: Adds NN to VX. (Carry flag is not changed)
+        case 0x7000: {
+            printf ("[!] add v%x, %#x\n");
+            V.at((opcode & 0x0F00) >> 8) += (uint8_t)(opcode & 0x00FF);
+            pc += 2;
+            break;
+        }
+
+        // Type: 8XY0
+        // Explanation: Sets VX to the value of VY.
+
+        // Type: 8XY1
+        // Explanation: Sets VX to VX or VY. (Bitwise OR operation)
+
+        // Type: 8XY2
+        // Explanation: Sets VX to VX and VY. (Bitwise AND operation)
+
+        // Type: 8XY3
+        // Explanation: Sets VX to VX xor VY.
+
+        // Type: 8XY4
+        // Explanation: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
+
+        // Type: 8XY5
+        // Explanation: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+
+        // Type: 8XY6
+        // Explanation: Stores the least significant bit of VX in VF and then shifts VX to the right by 1.[2]
+
+        // Type: 8XY7
+        // Explanation: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+
+        // Type: 8XYE
+        // Explanation: Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
+        case 0x8000: {
+            uint8_t lastDigit = opcode & 0x000F;
+            switch (lastDigit) {
+                case 0x0: {
+                    V.at((opcode & 0x0F00) >> 8) = V.at((opcode & 0x00F0) >> 8);
+                    pc += 2;
+                    break;
+                }
+                case 0x1: {
+                    V.at((opcode & 0x0F00) >> 8) | V.at((opcode & 0x00F0) >> 8);
+                    pc += 2;
+                    break;
+                }
+                case 0x2: {
+                    V.at((opcode & 0x0F00) >> 8) & V.at((opcode & 0x00F0) >> 8);
+                    pc += 2;
+                    break;
+                }
+                case 0x3: {
+                    V.at((opcode & 0x0F00) >> 8) ^ V.at((opcode & 0x00F0) >> 8);
+                    pc += 2;
+                    break;
+                }
+                // (TODO) Fix this.
+                case 0x4: {
+                    uint8_t tmp = (V.at((opcode & 0x0F00) >> 8) != 0) ? V.at((opcode & 0x0F00) >> 8 : V.at((opcode & 0x00F0) >> 8;
+                    V.at((opcode & 0x0F00) >> 8) += V.at((opcode & 0x00F0) >> 8);
+                    // If register overlaps, result is less than both of the operands.
+                    if (V.at((opcode & 0x0F00) >> 8) < tmp) {
+                        V.at(0xF) = 1;
+                    } else {
+                        V.at(0xF) = 0;
+                    }
+                    pc += 2;
+                    break;
+                }
+                case 0x5: {
+                    if (V.at((opcode & 0x0F00) >> 8) < V.at((opcode & 0x00F0) >> 8)) {
+                        V.at(0xF) = 1;
+                    } else {
+                        V.at(0xF) = 0;
+                    }
+                    V.at((opcode & 0x0F00) >> 8) -= V.at((opcode & 0x00F0) >> 8);
+                    pc += 2;
+                    break;
+                }
+                case 0x6: {
+                    break;
+                }
+                case 0x7: {
+                    break;
+                }
+                case 0xE: {
+                    break;
+                }
+            }
+
+            break;
+        }
+        case 0x9000: {
             printf ("[!] Success!\n");
             if (V.at((opcode & 0x0F00) >> 8) == V.at((opcode & 0x00F0) >> 8)) {pc += 0x4;}
             break;
-
-        default:
+        }
+        case 0xA000: {
+            printf ("[!] Success!\n");
+            if (V.at((opcode & 0x0F00) >> 8) == V.at((opcode & 0x00F0) >> 8)) {pc += 0x4;}
+            break;
+        }
+        default: {
             printf ("[!] Opcode: %#0.4x\n", opcode);
             throw std::runtime_error ("[-] Opcode is not implemented\n");
+        }
     }
 };
 
@@ -120,5 +243,8 @@ void chip8::postStep() {
 void chip8::debugPrint() {
     printf("pc_value: %#0.4x\n", pc);
     printf("sp_value: %#0.4x, stack_level: %d\n", stack[sp], sp);
+    printf("v0: %#0.2x\n", V.at(0x0));
+    printf("v1: %#0.2x\n", V.at(0x1));
+    printf("vf: %#0.2x\n", V.at(0xf));
 }
 
--- main.cc
+++ main.cc
@@ -6,6 +6,9 @@ int main(int argc, const char **argv) {
     chip8 console;
     std::cout << "[*] Starting program" << std::endl;
     console.loadRom("test");
+/*
+
+Program 0x0: Jumping randomly
     console.memoryLayout[0x200] = 0x02;
     console.memoryLayout[0x201] = 0x20;
     console.memoryLayout[0x202] = 0x02;
@@ -20,10 +23,44 @@ int main(int argc, const char **argv) {
     console.memoryLayout[0x240] = 0x00;
     console.memoryLayout[0x241] = 0xEE;
 
-    for (uint8_t i; i < 6; ++i) {
+Program 0x1:  Jumping over code if equal to immidiate
+    console.memoryLayout[0x200] = 0x60;
+    console.memoryLayout[0x201] = 0x77;  // mov v0, 0x77
+    console.memoryLayout[0x202] = 0x30;
+    console.memoryLayout[0x203] = 0x77;  // je(v0, 0x77) 0x2
+    console.memoryLayout[0x204] = 0x61;
+    console.memoryLayout[0x205] = 0x66;  // mov v0, 0x66
+    console.memoryLayout[0x206] = 0x61;
+    console.memoryLayout[0x207] = 0x99;  // mov v1, 0x99
+    console.memoryLayout[0x208] = 0x61;
+    console.memoryLayout[0x209] = 0x99;  // mov v1, 0x99
+
+Program 0x2:  Jumping over code if equal registers
+    console.memoryLayout[0x200] = 0x60;
+    console.memoryLayout[0x201] = 0x77;  // mov v0, 0x77
+    console.memoryLayout[0x202] = 0x61;
+    console.memoryLayout[0x203] = 0x76;  // mov v0, 0x77
+    console.memoryLayout[0x204] = 0x51;
+    console.memoryLayout[0x205] = 0x00;  // je(v1, v0) 0x4
+    console.memoryLayout[0x206] = 0x60;
+    console.memoryLayout[0x207] = 0x33;  // mov v0, 0x33
+    console.memoryLayout[0x208] = 0x61;
+    console.memoryLayout[0x209] = 0x99;  // mov v1, 0x99
+    console.memoryLayout[0x20A] = 0x61;
+    console.memoryLayout[0x20B] = 0x99;  // mov v1, 0x99
+
+Program 0x3: Adding some values to registers
+    console.memoryLayout[0x200] = 0x70;
+    console.memoryLayout[0x201] = 0x01;  // mov v0, 0x77
+    console.memoryLayout[0x202] = 0x70;
+    console.memoryLayout[0x203] = 0x02;  // mov v0, 0x77
+    console.memoryLayout[0x204] = 0x71;
+    console.memoryLayout[0x205] = 0x06;  // je(v1, v0) 0x4
+
+*/
+    for (uint8_t i; i < 4; ++i) {
         console.opcodeExecute();
     }
-
     return 0;
 }
 
